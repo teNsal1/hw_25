245cbfc76279f8175027d15d67cf58458b162dc4
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-05 15:16:38 +0300

    hw_25: добавлены доверенные источники CSRF

--- barbershop_project/barbershop/settings.py
+++ barbershop_project/barbershop/settings.py
@@ -85,3 +85,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
--- barbershop_project/barbershop/urls.py
+++ barbershop_project/barbershop/urls.py
@@ -1,4 +1,4 @@
-# barbershop/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
